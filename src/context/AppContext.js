81b423bd5b3973043702ca8d1af457f11ed8e7b0
Jai Sharma <jaiprakash.sharma44@gmail.com>
2020-03-19 18:48:45 +0530

    updated

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-let context = {
-    initProp: {
-        loading: true,
-        isAuthenticated: false,
-    },
+const context = {
+    loading: true,
+    isAuthenticated: false,
+    nightMode: false,
+    language: 'en',
+};
+context.PropTypes = {
+    loading: PropTypes.bool,
+    isAuthenticated: PropTypes.bool,
+    nightMode: PropTypes.bool,
+    language: PropTypes.string,
 };
 
 const AppContext = React.createContext(context);
